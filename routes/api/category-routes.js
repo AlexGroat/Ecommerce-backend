0dfe59df9106306cd8db3ac4cc01baf77d592ad0
Alex Groat <alex.groat.95@gmail.com>
2021-12-04 17:36:16 +1030

    comments for category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,24 +5,29 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
+  // category. FIND ALL
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  // category FIND ONE
   // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  // category. CREATE
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  // category. UPDATE
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  // category. DESTROY
 });
 
 module.exports = router;
