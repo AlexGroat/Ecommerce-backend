fd50997930aefb5075a07002ddc7698564875969
Alex Groat <alex.groat.95@gmail.com>
2021-12-05 23:35:11 +1030

    single category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -13,7 +13,7 @@ router.get('/', async (req, res) => {
     });
 
     if (!catData) {
-      res.status(404).json({ message: 'No category found with this id!'});
+      res.status(404).json({ message: 'No categories found with this id!'});
       return;
     }
     
@@ -23,10 +23,24 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
+router.get('/:id', async (req, res) => {
+ // find one category by its `id` value
   // category FIND ONE
   // be sure to include its associated Products
+  try {
+    const singleCatData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    });
+
+    if (!singleCatData) {
+      res.status(404).json({ message: 'No category found with this id!'});
+      return;
+    }
+    
+    res.status(200).json(singleCatData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
