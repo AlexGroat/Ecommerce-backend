1876db0c150291b5688abcfd62ff3d89cd6e51b2
Alex Groat <alex.groat.95@gmail.com>
2021-12-06 00:24:02 +1030

    Get single product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -28,6 +28,21 @@ router.get('/', async (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const singleProdData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, {model: Tag}]
+    });
+
+    if (!singleProdData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(singleProdData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
 // create new product
